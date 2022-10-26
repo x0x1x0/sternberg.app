167c308a3aa4bdbb956afd64842ece50b42c4298
Jan Sternberg <janlucas.sternberg@gmail.com>
2022-10-26 23:40:44 +0200

    test

--- src/Landing/Homesection.jsx
+++ src/Landing/Homesection.jsx
@@ -6,8 +6,9 @@ function Homesection () {
     return (
 <>
 <section id="sec1">
+
+
         
-        <img src={MyImage} id="bg" />
         <h1 class="hidden mx-5">Welcome to my page</h1>
         <p class="hidden mx-5">mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur</p>
 </section>
