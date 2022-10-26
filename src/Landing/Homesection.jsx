22526c1c8f0530b81e852de1ad7a30fecedfdd37
Jan Sternberg <janlucas.sternberg@gmail.com>
2022-10-27 00:05:20 +0200

    breakpoints

--- src/Landing/Homesection.jsx
+++ src/Landing/Homesection.jsx
@@ -6,11 +6,10 @@ function Homesection () {
     return (
 <>
 <section id="sec1">
-
-
-        
-        <h1 class="hidden mx-5">Welcome to my page</h1>
+<div class="textdiv1">
+<h1 class="hidden mx-5">Welcome to my page</h1>
         <p class="hidden mx-5">mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur</p>
+</div>   
 </section>
     <section  id="sec2">
         <h2 class="hidden mx-5">Und halt helly</h2>
