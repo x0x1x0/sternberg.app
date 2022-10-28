7b0e29edf450586cc7af96d31f10c30c0e03697c
Jan Sternberg <janlucas.sternberg@gmail.com>
2022-10-28 23:35:44 +0200

    test

--- src/Landing/Homesection.jsx
+++ src/Landing/Homesection.jsx
@@ -8,12 +8,12 @@ function Homesection () {
 <section id="sec1">
 <div class="textdiv1">
 <h1 class="hidden mx-5">Welcome to my page</h1>
-        <p class="hidden mx-5">mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur mehr funny stuff Lorem ipsum dolor sit amet consectetur</p>
+        <p class="hidden mx-5">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. </p>
 </div>   
 </section>
     <section  id="sec2">
-        <h2 class="hidden mx-5">Und halt helly</h2>
-        <p class="hidden mx-5">mehr funny stuff Lorem ipsum dolor sit amet consectetur</p>
+        <h2 class="hidden mx-5">And so on</h2>
+        <p class="hidden mx-5">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden.</p>
     </section>
 
 
